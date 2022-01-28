a85a4f3ea55f61ae9c455d1d965a0a0765d367ee
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-28 15:32:38 +0100

    added object

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ console.log('Vue Ok!', Vue);
 Vue.config.devtools = true;
 
 const message = new Vue({
-    
+
     el: '#root',
 
+    data: {
+    tasks: [
+        {text: 'Fare la spesa a Zia May', done: true,},
+        {text: 'Portare Mary Jane sopra la Statua della Libertà', done: false,},
+        {text: 'Mangiare una Pizza al SALAMINO da Frank', done: false,},
+        {text: 'Scattare un selfie con Russel Crowe', done: false,},
+    ],
+    }
 });
